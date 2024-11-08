41bd48224c46cad3a426600c1406b168b8b3452c
Tcccgit <00001102148040sp@al.educacao.sp.gov.br>
2024-11-08 12:00:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ciencias?',
+    'o que as plantas precisam para crescer cite 2?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'se você tem 5 maças e ganha mais 3, quantas maçãs você tem agora?',
+    '8 maçãs'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
